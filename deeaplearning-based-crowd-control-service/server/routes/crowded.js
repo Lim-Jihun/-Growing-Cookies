1b440631768883a18f647dc2d20dd3f2b298a9a6
백종호 <155928591+baekdori@users.noreply.github.com>
2024-05-31 10:57:49 +0900

    혼잡도 상위 5개 구역 그래프 db 연결 수정 + db.js 호스트명 수정

--- deeaplearning-based-crowd-control-service/server/routes/crowded.js
+++ deeaplearning-based-crowd-control-service/server/routes/crowded.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const router = express.Router()
 const AnalyzeInfo = require('../model/analyze_info');
+/* router가 없어서 추가했습니다 */
 
 // * test용 아이디 
-userID = 'user1';
+userId = 'user1';
 exhb_id = 'exhb1';
 
 // front에서 전시관 클릭시 전시관 id를 넘겨야함
@@ -11,8 +13,8 @@ exhb_id = 'exhb1';
 
 
 router.get('/query', (req, res) => {
-  const { userID, exhbId } = req.query;
-  AnalyzeInfo.topCrowded(userID, exhbId, (err, results) => {
+  const { userId, exhbId } = req.query;
+  AnalyzeInfo.topCrowded(userId, exhbId, (err, results) => {
     if (err) {
       console.error('Error fetching analyze info:', err);
       return res.status(500).json({ error: 'Internal Server Error' });
