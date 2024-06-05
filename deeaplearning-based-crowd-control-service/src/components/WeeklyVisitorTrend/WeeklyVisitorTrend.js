7f2301ccc793695d6a0d6b9f9c13610c622267fd
Jiwon.C <tyfg0908@gmail.com>
2024-06-05 17:31:59 +0900

    지원: 일주일추이 숫자랑 아이콘 안뜨는 문제 해결

--- deeaplearning-based-crowd-control-service/src/components/WeeklyVisitorTrend/WeeklyVisitorTrend.js
+++ deeaplearning-based-crowd-control-service/src/components/WeeklyVisitorTrend/WeeklyVisitorTrend.js
@@ -18,113 +18,116 @@ const WeeklyVisitorTrend = ({
 
   const [parsedData1, setParseData1] = useState([]);
   const [parsedData2, setParseData2] = useState([]);
-  const [icons, setIcons] = useState({ icon1: null, icon2: null });
-  const [twvisitor, setTwVisitor] = useState([]);
-  const [lwvisitor, setLwVisitor] = useState([]);
-  const [llwvisitor, setLLwVisitor] = useState([]);
+  const [icon1, setIcon1] = useState([]);
+  const [icon2, setIcon2] = useState([]);
+  const [twvisitor, setTwvisitor] = useState(weekavg.this_week);
+  const [lwvisitor, setLwvisitor] = useState(weekavg.last_week);
+  const [llwvisitor, setLlwvisitor] = useState(weekavg.last_month);
 
-  
+  console.log("icon1", icon1)
+
   
   useEffect(() => {
 
-    setTwVisitor(weekavg.this_week);
-    setLwVisitor(weekavg.last_week);
-    setLLwVisitor(weekavg.last_month);
+    const setWeekAvg = () => {
+    setTwvisitor(weekavg.this_week);
+    setLwvisitor(weekavg.last_week);
+    setLlwvisitor(weekavg.last_month);
 
-    let icon1 = (
+    }
+
+    setWeekAvg();
+
+    const interval = setInterval(setWeekAvg, 60000);
+
+    return () => clearInterval(interval);
+  }, [weekavg]);
+
+
+  // icon선언
+  const upIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="#EC5454"
+      class="bi bi-triangle-fill"
+      viewBox="0 0 16 16"
+      style={{ marginLeft: "2rem" }}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+      />
+    </svg>
+  );
+
+const downIcon = (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="25"
+        width="16"
+        height="16"
         fill="#3498DB"
-        class="bi bi-dash"
+        class="bi bi-triangle-fill"
         viewBox="0 0 16 16"
+        style={{ marginLeft: "2rem", transform: "rotate(60deg)" }}
       >
-        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+        <path
+          fill-rule="evenodd"
+          d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
+        />
       </svg>
     );
-    let icon2 = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="25"
-        fill="#3498DB"
-        class="bi bi-dash"
-        viewBox="0 0 16 16"
-      >
-        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
-      </svg>
-    );
-    if (twvisitor > lwvisitor) {
-      icon1 = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="#EC5454"
-          class="bi bi-triangle-fill"
-          viewBox="0 0 16 16"
-          style={{ marginLeft: "2rem" }}
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
-          />
-        </svg>
-      );
-    } else {
-      icon1 = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="#3498DB"
-          class="bi bi-triangle-fill"
-          viewBox="0 0 16 16"
-          style={{ marginLeft: "2rem", transform: "rotate(60deg)" }}
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
-          />
-        </svg>
-      );
+
+    let dashIcon =(
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="#5F9F41"
+            class="bi bi-dash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+          </svg>
+        );
+
+  // 값에 따라 icon 설정
+  useEffect(() => {
+
+    const setIcon = () =>{
+    if (twvisitor > lwvisitor ){
+      setIcon1(upIcon);
+    }else if (twvisitor< lwvisitor) {
+      setIcon1(downIcon);
+    }else{
+      setIcon1(dashIcon);
     }
-    if (lwvisitor > llwvisitor) {
-      icon2 = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="#EC5454"
-          class="bi bi-triangle-fill"
-          viewBox="0 0 16 16"
-          style={{ marginLeft: "2rem" }}
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
-          />
-        </svg>
-      );
-    } else {
-      icon2 = (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="#3498DB"
-          class="bi bi-triangle-fill"
-          viewBox="0 0 16 16"
-          style={{ marginLeft: "2rem", transform: "rotate(60deg)" }}
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.022 1.566a1.13 1.13 0 0 1 1.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767z"
-          />
-        </svg>
-      );
+
+    if (lwvisitor > llwvisitor ){
+      setIcon2(upIcon);
+    }else if(lwvisitor<llwvisitor){
+      setIcon2(downIcon);
+    }else{
+      setIcon2(dashIcon);
     }
+
+  }
+  setIcon();
+
+    const interval = setInterval(setIcon, 60000);
+
+    return () => clearInterval(interval);
+  });
+  
+
+
+  console.log("twvisitor",twvisitor, typeof(twvisitor));
+  console.log("lwvisitor",lwvisitor, typeof(lwvisitor));
+  
+  
+
+  useEffect(() => {
 
     const fetchData = async () => {
       try {
@@ -134,18 +137,20 @@ const WeeklyVisitorTrend = ({
           console.error("세션에서 userID를 가져올 수 없습니다.");
           return;
         }
-
+        //이번주 일주일간 방문인원 데이터 요청
         const response = await axios.get(`http://localhost:4000/thisweek`, {
           params: { userId }, // 쿼리스트링으로 userId 전달
           withCredentials: true,
         });
-     
+        
+        //저번주 일주일간 방문인원 데이터 요청
         const response2 = await axios.get(`http://localhost:4000/lastweek`, {
           params: { userId }, // 쿼리스트링으로 userId 전달
           withCredentials: true,
         });
         if (response.status === 200) {
           console.log("이번주 추이", response.data);
+          // 이번주 일주일간 날짜와 방문인원 전달
           const parsedData1 = response.data.map(d => ({
             day: new Date(d.day),
             avg_population: +d.avg_population
@@ -154,7 +159,7 @@ const WeeklyVisitorTrend = ({
         }
         if (response2.status === 200) {
           console.log("저번주 추이", response2.data);
-
+          // 저번주 일주일간 날짜와 방문인원 전달
           const parsedData2 = response2.data.map(d => ({
             day: new Date(d.day),
             avg_population: +d.avg_population
@@ -171,7 +176,7 @@ const WeeklyVisitorTrend = ({
   
  
 
-  setIcons({ icon1, icon2 });
+  
 
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -186,11 +191,14 @@ const WeeklyVisitorTrend = ({
     };
 
     fetchData();
+    
+  
 
     const interval = setInterval(fetchData, 60000);
 
     return () => clearInterval(interval); // 컴포넌트가 unmount될 때 interval 해제
   }, []);
+
   return (
     <div className={styles.VisitorTrend}>
       <table className={styles.visitor}>
@@ -212,7 +220,7 @@ const WeeklyVisitorTrend = ({
                 <div className={styles.numCol}>
                   <p className={styles.num}>{twvisitor}</p>
 
-                  <div className={styles.arrawCol}>{icons.icon1}</div>
+                  <div className={styles.arrawCol}>{icon1}</div>
                 </div>
               </div>
               <div className={styles.numOrGuideRaw}>
@@ -242,7 +250,7 @@ const WeeklyVisitorTrend = ({
                 <div className={styles.numCol}>
                   <p className={styles.num}>{lwvisitor}</p>
                 </div>
-                <div className={styles.arrawCol}>{icons.icon2}</div>
+                <div className={styles.arrawCol}>{icon2}</div>
               </div>
               <div className={styles.numOrGuideRaw}>
                 <div className={styles.guide}>
